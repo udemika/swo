00c8bc8cdb24bae56fa92a26b4ead0157ce50c0f
udemika <aksarus@gmail.com>
2026-01-15 21:28:25 +0500

    Add files via upload

--- swo.js
+++ swo.js
@@ -6,210 +6,181 @@
         if (window.showypro_plugin_loaded) return;
         window.showypro_plugin_loaded = true;
 
-        console.log('[ShowyPro] v11.0 loading...');
+        console.log('[ShowyPro] v13.0 SUPER FIX');
 
-        var WORKING_UID = 'i8nqb9vw';
-        var WORKING_TOKEN = 'f8377057-90eb-4d76-93c9-7605952a096l';
-        var BASE_DOMAIN = 'showypro.com/lite/fxapi';
-
-        var PROXIES = [
-            'https://cors.byskaz.ru/',
-            'http://85.198.110.239:8975/',
-            'http://91.184.245.56:8975/',
-            'https://apn10.akter-black.com/',
-            'https://apn5.akter-black.com/',
-            'https://cors557.deno.dev/'
-        ];
-
-        var Network = Lampa.Request || Lampa.Reguest;
-
-        function sign(url) {
-            if (url.indexOf('uid=') == -1) url = Lampa.Utils.addUrlComponent(url, 'uid=' + WORKING_UID);
-            if (url.indexOf('showy_token=') == -1) url = Lampa.Utils.addUrlComponent(url, 'showy_token=' + WORKING_TOKEN);
-            return url;
-        }
+        var uid = 'i8nqb9vw';
+        var token = 'f8377057-90eb-4d76-93c9-7605952a096l';
+        var api = 'showypro.com/lite/fxapi';
+        var cors = 'https://cors.byskaz.ru/';
 
         Lampa.Listener.follow('full', function(e) {
             if (e.type == 'complite') {
-                var btn = $('<div class="full-start__button selector" style="margin-left:1rem;"><div style="font-size:1.2em;padding:1rem;">ShowyPro</div></div>');
-                btn.on('hover:enter', function() {
-                    Lampa.Component.add('showypro', showypro_component);
+                var btn = document.createElement('div');
+                btn.className = 'full-start__button selector';
+                btn.style.marginLeft = '1rem';
+                btn.innerHTML = '<div style="font-size:1.2em;padding:1rem;">ShowyPro</div>';
+
+                btn.addEventListener('hover:enter', function() {
+                    Lampa.Component.add('showypro', component);
                     Lampa.Activity.push({
                         url: '',
                         title: 'ShowyPro',
                         component: 'showypro',
-                        movie: e.data.movie,
-                        page: 1
+                        movie: e.data.movie
                     });
                 });
-                e.object.activity.render().find('.view--torrent').after(btn);
-                console.log('[ShowyPro] Button OK');
+
+                e.object.activity.render().find('.view--torrent')[0].after(btn);
             }
         });
 
-        var showypro_component = function(object) {
-            var network = new Network();
+        var component = function(object) {
+            var _this = this;
+            var network = new Lampa.Request();
             var scroll = new Lampa.Scroll({mask: true, over: true});
-            var html = $('<div></div>');
+            var html = document.createElement('div');
             var last;
 
-            var current_kinopoisk_id = null;
+            var kp_id = null;
 
             this.create = function() {
-                console.log('[ShowyPro] create()');
-                html.append(scroll.render());
-                scroll.append('<div style="padding:2rem;text-align:center;color:#999">Загрузка...</div>');
+                console.log('[ShowyPro] CREATE v13');
+                html.appendChild(scroll.render());
+                _this.loading();
+
                 Lampa.Controller.add('content', {
-                    toggle: function() { 
+                    toggle: function() {
                         Lampa.Controller.collectionSet(scroll.render());
                         Lampa.Controller.collectionFocus(last, scroll.render());
                     },
-                    right: function() { Lampa.Controller.toggle('menu'); },
-                    left: function() { if (Navigator.canmove('left')) Navigator.move('left'); else Lampa.Controller.toggle('menu'); },
-                    back: this.back
+                    back: function() { Lampa.Activity.backward(); }
                 });
-                this.activityLoader();
+
                 return html;
             };
 
             this.start = function() {
-                console.log('[ShowyPro] start()');
+                console.log('[ShowyPro] START v13');
                 Lampa.Controller.toggle('content');
+                _this.load();
             };
 
-            this.activityLoader = function() {
-                if (object.movie.kinopoisk_id || object.movie.kp_id) {
-                    current_kinopoisk_id = object.movie.kinopoisk_id || object.movie.kp_id;
-                    var url = 'http://' + BASE_DOMAIN + '?kinopoisk_id=' + current_kinopoisk_id;
-                    if (object.movie.title) url = Lampa.Utils.addUrlComponent(url, 'title=' + encodeURIComponent(object.movie.title));
-                    url = sign(url);
+            this.loading = function() {
+                scroll.clear();
+                var div = document.createElement('div');
+                div.style.padding = '4rem';
+                div.style.textAlign = 'center';
+                div.style.color = '#aaa';
+                div.style.fontSize = '1.1em';
+                div.textContent = '🔄 Загрузка ShowyPro...';
+                scroll.append(div);
+            };
 
-                    console.log('[ShowyPro] URL:', url);
-                    this.requestProxy(url);
-                } else {
-                    scroll.clear().append('<div style="padding:4rem;text-align:center;color:#999;font-size:1.2em">Нет Kinopoisk ID</div>');
+            this.load = function() {
+                if (!object.movie.kinopoisk_id && !object.movie.kp_id) {
+                    _this.error('Нет ID фильма');
+                    return;
                 }
-            };
 
-            this.requestProxy = function(url, success, error) {
-                var proxy = PROXIES[0]; // Только первый рабочий
-                var fullUrl = proxy + url;
+                kp_id = object.movie.kinopoisk_id || object.movie.kp_id;
+                var url = 'http://' + api + '?kinopoisk_id=' + kp_id;
+                if (object.movie.title) {
+                    url = Lampa.Utils.addUrlComponent(url, 'title=' + encodeURIComponent(object.movie.title));
+                }
+                url = Lampa.Utils.addUrlComponent(url, 'uid=' + uid);
+                url = Lampa.Utils.addUrlComponent(url, 'showy_token=' + token);
 
-                console.log('[ShowyPro] Fetch:', fullUrl);
-
-                network.native(fullUrl, (html) => {
-                    console.log('[ShowyPro] HTML length:', html.length);
-                    if (html.length > 500) {
-                        this.parseHTML(html);
-                    } else {
-                        this.empty('Пустой ответ сервера');
-                    }
-                }, () => {
-                    this.empty('Ошибка сети');
+                console.log('[ShowyPro] LOAD:', url);
+                network.timeout(10000);
+                network.native(cors + url, function(html) {
+                    console.log('[ShowyPro] OK:', html.length);
+                    _this.parseHTML(html);
+                }, function() {
+                    _this.error('Сервер недоступен');
                 }, false, {dataType: 'text'});
             };
 
             this.parseHTML = function(html) {
-                console.log('[ShowyPro] Parsing...');
                 try {
-                    var $dom = $('<div>').html(html);
+                    var parser = new DOMParser();
+                    var doc = parser.parseFromString(html, 'text/html');
+                    var items = doc.querySelectorAll('.videos__item.videos__season, .videos__item.videos__movie');
 
-                    // Похожие фильмы
-                    var similarItems = $dom.find('.videos__item.videos__season');
-                    if (similarItems.length) {
-                        console.log('[ShowyPro] Similar movies:', similarItems.length);
-                        this.showList(similarItems, 'Похожие фильмы', (item) => {
-                            var data = item.attr('data-json');
-                            try {
-                                var json = JSON.parse(data);
-                                if (json.url) {
-                                    var postid = json.url.match(/postid=(\d+)/);
-                                    if (postid) this.loadContent('postid=' + postid[1]);
-                                }
-                            } catch(e) {}
-                        });
-                        return;
-                    }
+                    console.log('[ShowyPro] Items:', items.length);
 
-                    // Эпизоды
-                    var episodes = $dom.find('.videos__item.videos__movie');
-                    console.log('[ShowyPro] Episodes:', episodes.length);
-                    if (episodes.length) {
-                        this.showEpisodes(episodes);
+                    if (items.length) {
+                        this.showList(items);
                     } else {
-                        this.empty('Контент не найден');
+                        this.error('Контент не найден');
                     }
                 } catch(e) {
-                    console.log('[ShowyPro] Parse error:', e);
-                    this.empty('Ошибка парсинга');
+                    console.log('[ShowyPro] Parse:', e);
+                    this.error('Ошибка разбора');
                 }
             };
 
-            this.showList = function(items, title, onclick) {
+            this.showList = function(items) {
                 scroll.clear();
-                if (items.length) {
-                    items.each((i, el) => {
-                        var $item = $(el);
-                        var text = $item.find('.videos__season-title,.videos__item-title').first().text().trim();
-                        var card = $(`<div class="selector" style="padding:1.2rem;border-bottom:1px solid #444"><div style="font-size:1.3em;color:white">${text}</div></div>`);
-                        card.on('hover:enter', () => onclick($item));
-                        card.on('hover:focus', (e) => last = e.target);
-                        scroll.append(card);
-                    });
-                } else {
-                    scroll.append(`<div style="padding:3rem;text-align:center;color:#999">${title} не найдены</div>`);
-                }
-            };
 
-            this.showEpisodes = function(items) {
-                scroll.clear();
-                items.each((i, el) => {
-                    var $item = $(el);
-                    var title = $item.find('.videos__item-title').text().trim();
-                    var data = $item.attr('data-json');
+                for (var i = 0; i < items.length; i++) {
+                    var item = items[i];
+                    var title = item.querySelector('.videos__season-title, .videos__item-title');
+                    if (!title) continue;
 
-                    var card = $(`<div class="selector" style="padding:1.2rem;border-bottom:1px solid #444"><div style="font-size:1.3em;color:white">${title}</div></div>`);
+                    var titleText = title.textContent.trim();
+                    var dataAttr = item.getAttribute('data-json');
 
-                    card.on('hover:enter', () => {
-                        try {
-                            var json = JSON.parse(data);
-                            if (json.url) {
-                                Lampa.Player.play(json.url);
-                            } else if (json.quality) {
-                                var q = Object.keys(json.quality)[0];
-                                Lampa.Player.play(json.quality[q]);
+                    var card = document.createElement('div');
+                    card.className = 'selector';
+                    card.style.padding = '1.5rem';
+                    card.style.borderBottom = '1px solid #333';
+                    card.style.background = 'rgba(0,0,0,0.4)';
+                    card.innerHTML = '<div style="font-size:1.4em;color:#fff;line-height:1.2">' + titleText + '</div>';
+
+                    card.addEventListener('hover:enter', (function(data, title) {
+                        return function() {
+                            if (data) {
+                                try {
+                                    var json = JSON.parse(data);
+                                    if (json.url) {
+                                        Lampa.Player.play(json.url);
+                                        return;
+                                    }
+                                    if (json.quality) {
+                                        var qs = Object.keys(json.quality);
+                                        if (qs.length) {
+                                            Lampa.Player.play(json.quality[qs[0]]);
+                                            return;
+                                        }
+                                    }
+                                } catch(e) {}
                             }
-                        } catch(e) {
-                            Lampa.Noty.show('Ошибка воспроизведения');
-                        }
+                            Lampa.Noty.show('Нет ссылки');
+                        };
+                    })(dataAttr, titleText));
+
+                    card.addEventListener('hover:focus', function(e) {
+                        last = e.target;
+                        scroll.update(card, true);
                     });
 
-                    card.on('hover:focus', (e) => last = e.target);
                     scroll.append(card);
-                });
+                }
             };
 
-            this.loadContent = function(params) {
-                scroll.clear().append('<div style="padding:2rem;text-align:center;color:#999">Загрузка...</div>');
-                var url = 'http://' + BASE_DOMAIN + '?' + params + '&kinopoisk_id=' + current_kinopoisk_id;
-                url = sign(url);
-                this.requestProxy(url);
-            };
-
-            this.empty = function(msg) {
-                scroll.clear().append('<div style="padding:4rem;text-align:center;color:#999;font-size:1.2em">' + msg + '</div>');
-            };
-
-            this.render = function() {
-                return html;
-            };
-
-            this.back = function() {
-                Lampa.Activity.backward();
+            this.error = function(msg) {
+                scroll.clear();
+                var div = document.createElement('div');
+                div.style.padding = '4rem';
+                div.style.textAlign = 'center';
+                div.style.color = '#ff6b6b';
+                div.style.fontSize = '1.1em';
+                div.textContent = '❌ ' + msg;
+                scroll.append(div);
             };
         };
 
-        console.log('[ShowyPro] v11.0 ready');
+        console.log('[ShowyPro] v13 LOADED');
     }
 
     if (window.appready) startPlugin();
